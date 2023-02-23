3dfab6e7edce1a0e06b07944b8d953809c0f479a
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-02-23 09:14:33 +0700

    implement api to get list files

--- reader/files/views.py
+++ reader/files/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from files.models import File
+from django.shortcuts import get_object_or_404
+from files.serializers import FileSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+
+class FileViewSet(viewsets.ViewSet):
+    """
+    A simple ViewSet for listing or retrieving users.
+    """
+    def list(self, request):
+        queryset = File.objects.all()
+        serializer = FileSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = File.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = FileSerializer(user)
+        return Response(serializer.data)
+
+
--- reader/reader/urls.py
+++ reader/reader/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from files.views import FileViewSet
+
+router = DefaultRouter()
+router.register(r'files', FileViewSet, basename='file')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
