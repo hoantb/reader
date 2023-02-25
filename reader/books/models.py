c5182d795682311faead4c9478eeed90114d6ed7
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-02-25 22:23:54 +0700

    update

--- reader/books/models.py
+++ reader/books/models.py
@@ -6,4 +6,6 @@ class Book(models.Model):
     title = models.TextField(null=False, blank=False)
     image_preview = models.ImageField(null=True, blank=True,upload_to='books')
     description = models.TextField(null=True, blank=True)
-    
+
+    def __str__(self):
+        return self.title 
--- reader/files/models.py
+++ reader/files/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from books.models import Book
 
 
 class File(models.Model):
     title = models.TextField(null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
     file = models.FileField(null=True, blank=True, upload_to='files')
     image_preview = models.ImageField(null=True, blank=True,upload_to='files')
+    book = models.ForeignKey(Book, null=True, blank=True, on_delete=models.SET_NULL, related_name='files')
     
+    def __str__(self):
+        return self.title 
