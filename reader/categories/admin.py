f5e3152628243846dbbbf68e93ce7427f7fc502c
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-06 14:26:07 +0700

    add category

--- reader/categories/admin.py
+++ reader/categories/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from categories.models import BookCategory
 
 # Register your models here.
+class BookCategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(BookCategory, BookCategoryAdmin)
