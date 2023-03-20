3a2d04819437d6235084900783f4113ef04166fa
hoan.bt <hoan.bt@sutrixsolutions.com>
2023-03-20 17:29:22 +0700

    change to psql

--- reader/reader/settings.py
+++ reader/reader/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'reader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ittruyen',
+        'USER': 'ittruyenadmin',
+        'PASSWORD': 'Traxanh123',
+        'HOST': '34.87.121.21',
+        'PORT': '5432',
     }
 }
 
